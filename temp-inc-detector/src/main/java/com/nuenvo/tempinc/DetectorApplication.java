d1f1431cfd7718dcaaf9d5495d6e8d73ffc3349f
thefrsh <mbroniewicz@outlook.com>
2024-01-27 16:52:47 +0100

    Init

---
+++ temp-inc-detector/src/main/java/com/nuenvo/tempinc/DetectorApplication.java
@@ -0,0 +1,13 @@
+package com.nuenvo.tempinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(DetectorApplication.class, args);
+  }
+}
