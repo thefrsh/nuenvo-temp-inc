caa54957c7b2be4224a2bf237c1e4876603533f1
Michal Broniewicz <mbroniewicz@outlook.com>
2024-01-27 18:55:30 +0100

    Swagger Init

---
+++ temp-inc-analytics-api/src/main/java/com/nuenvo/tempinc/infrastructure/SpringDocConfiguration.java
@@ -0,0 +1,40 @@
+package com.nuenvo.tempinc.infrastructure;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class SpringDocConfiguration {
+
+  @Value(value = "${project.version}")
+  private String projectVersion;
+
+  @Bean
+  GroupedOpenApi groupedOpenApi() {
+
+    return GroupedOpenApi.builder()
+      .group("Query")
+      .packagesToScan("com.nuenvo.tempinc.query")
+      .build();
+  }
+
+  @Bean
+  OpenAPI openAPI() {
+
+    return new OpenAPI()
+      .info(new Info()
+        .title("'Always Right Temp Inc' temperature anomalies solution")
+        .description("The masters of temperature sensors!")
+        .version(projectVersion)
+      ).externalDocs(
+        new ExternalDocumentation()
+          .description("README.md")
+          .url("https://github.com/thefrsh/nuenvo-temp-inc/blob/master/README.md")
+      );
+  }
+}
